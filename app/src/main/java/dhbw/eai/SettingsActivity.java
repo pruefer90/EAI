f4e8f67535324209dd1baf014251fb6ea8785e15
Manu <wagenbach.m@gmail.com>
2017-06-24 12:11:51 +0200

    SharedPreferences + GPS

--- app/src/main/java/dhbw/eai/SettingsActivity.java
+++ app/src/main/java/dhbw/eai/SettingsActivity.java
@@ -5,16 +5,47 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.security.acl.Group;
+
+import static dhbw.eai.R.id.save;
+import static dhbw.eai.R.id.thing_proto;
 
 /**
  * Created by mathz on 19.06.2017.
  */
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText timeText;
+    private EditText linkText;
+    private RadioGroup wayGroup;
+    private RadioButton wayButton1;
+    private RadioButton wayButton2;
+    private RadioButton wayButton3;
+    private RadioButton wayButton4;
+    private Button saveButton;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        timeText = (EditText) findViewById(R.id.time);
+        linkText = (EditText) findViewById(R.id.link);
+        wayGroup = (RadioGroup) findViewById(R.id.waygroup);
+        wayButton1 = (RadioButton) findViewById(R.id.way1);
+        wayButton2 = (RadioButton) findViewById(R.id.way2);
+        wayButton3 = (RadioButton) findViewById(R.id.way3);
+        wayButton4 = (RadioButton) findViewById(R.id.way4);
+        saveButton = (Button) findViewById(R.id.save);
+
+        saveButton.setOnClickListener(this);
     }
 
     @Override
@@ -32,6 +63,16 @@ public class SettingsActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == saveButton){
+            String time = timeText.getText().toString();
+            if (!time.isEmpty()){
+
+            }
         }
     }
 }
---
+++ app/src/main/java/dhbw/eai/background/SaveSharedPreference.java
@@ -0,0 +1,66 @@
+package dhbw.eai.background;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by Manuel Wagenbach on 24.06.2017.
+ */
+
+public class SaveSharedPreference {
+    static final String pref_time = "time";
+    static final String pref_link = "link";
+    static final String pref_way = "way";
+
+    static SharedPreferences getSharedPreferences(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void setPrefTime(Context context, int time){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putInt(pref_time, time);
+        editor.commit();
+    }
+
+    public static void setPrefLink(Context context, String link){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putString(pref_link, link);
+        editor.commit();
+    }
+
+    public static void setPrefWay(Context context, int way){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putInt(pref_way, way);
+        editor.commit();
+    }
+
+    public static void setAll(Context context, int time, String link, int way){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.putInt(pref_time, time);
+        editor.putString(pref_link, link);
+        editor.putInt(pref_way, way);
+        editor.commit();
+    }
+
+    public static int getPrefTime(Context context){
+        return getSharedPreferences(context).getInt(pref_time, 0);
+    }
+
+    public static int getPrefWay(Context context){
+        return getSharedPreferences(context).getInt(pref_way, 0);
+    }
+
+    public static String getPrefLink(Context context){
+        return getSharedPreferences(context).getString(pref_link, "");
+    }
+
+    public static void clear(Context context){
+        SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+        editor.remove(pref_time);
+        editor.remove(pref_link);
+        editor.remove(pref_way);
+        editor.commit();
+    }
+}
